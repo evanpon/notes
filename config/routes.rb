a6b0773d03d7dfb7183d73fef37be224a6d6c373
Evan Pon <evanpon@gmail.com>
2014-04-26 12:38:52 -0700

    create tags and notes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'notes#index'
+  resources :tags do
+    member do
+      get 'notes'
+    end
+  end
+  resources :notes
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
